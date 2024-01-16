260ae26ab3d97025e841a306757eaf25037dc5d7
breno.breves <brenocsb@dcc.ufrj.br>
2024-01-16 18:19:32 -0300

    Exercício 4

--- Mult/src/App.java
+++ Mult/src/App.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args){
         Scanner myInput = new Scanner( System.in );
-        System.out.print( "Enter the number to multiply: " );
-        int num = myInput.nextInt();
+        System.out.print( "Enter the piramid height: " );
+        int height = myInput.nextInt();
         myInput.close();
-        System.out.println("Tabela de multiplicação por "+ num);
-        for( int i = 1 ; i <= 10 ; i++){
-            System.out.println(num + " x " + i + " = " + num*i);
+        for( int i = 1 ; i <= height ; i++){
+            for( int j = 0 ; j < i ; j++){
+                System.out.print(i);
+            }
+            System.out.println("");
         }
     }
 }
