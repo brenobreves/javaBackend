9e0c216e6853daace52c9e9a303321fad47daa0a
breno.breves <brenocsb@dcc.ufrj.br>
2024-01-16 18:04:30 -0300

    Exercício 2

---
+++ Fib/src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Scanner myInput = new Scanner( System.in );
+        System.out.print( "Enter the number of fibs: " );
+        int amount = myInput.nextInt();
+        int fib1 = 1;
+        int fib2 = 1;
+        for( int i = 0 ; i < amount ; i++){
+            if(i<2){
+                System.out.print(1+ " ");
+                continue;
+            }
+            int next = fib1 + fib2;
+            fib1 = fib2;
+            fib2 = next;
+            System.out.print(fib2+" ");
+        }
+        System.out.println("");
+    }
+}
