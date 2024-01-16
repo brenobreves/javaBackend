260e92fc743dbe43f625e35161884c8efbe65767
breno.breves <brenocsb@dcc.ufrj.br>
2024-01-16 17:49:19 -0300

    Primeiro exercício

---
+++ O_que_e_esse_num/src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Scanner myInput = new Scanner( System.in );
+        System.out.print( "Enter the number: " );
+        int num = myInput.nextInt();
+        String signal = "";
+        String tipo = "";
+        if (num % 2 == 0){
+            tipo = "par";
+        } else{
+            tipo = "impar";
+        }
+        if (num < 0){
+            signal = "negativo";
+        } else{
+            signal = "positivo";
+        }
+        System.out.println(num + " é " + tipo + " e " + signal);
+    }
+}
